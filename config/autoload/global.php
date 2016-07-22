360b9e3f3d2d2ac822e5a6048f8d53090643571f
Fahad Farrukh <fahad.farrukh@nxb.com.pk>
2016-07-22 20:42:00 +0500

    Getting Started with Zend Framework 2 Simple inventory system to display which albums we own. It lists our collection and allow us to add, edit and delete CDs.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Global Configuration Override
  *
@@ -12,5 +13,17 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=zf2tutorial;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+            => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
